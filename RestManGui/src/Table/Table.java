d53eb05901aa65b08fbbbc56ba224ed8d5252434
Gimlie102 <garrettlaman@gmail.com>
2019-11-30 17:08:27 -0500

    Table class, needs table controller

--- RestManGui/src/Table/Table.java
+++ RestManGui/src/Table/Table.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Table;
-
+import Employee.*;
 /**
  *
  * @author garre
  */
 public class Table {
+    
+    private String name;
+    private int chairs;
+    private boolean occupied;
+    private Employee assignedWaiter;
+
+    public Table(String name, int chairs, boolean occupied, Employee assignedWaiter) {
+        this.name = name;
+        this.chairs = chairs;
+        this.occupied = occupied;
+        this.assignedWaiter = assignedWaiter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChairs() {
+        return chairs;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+    
+    public Employee getAssignedWaiter() {
+        return assignedWaiter;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
     
 }
