7c45ece41b24bcf0b59736957834d49b9f60a244
Ethan Norlander <ethannor@gmail.com>
2019-11-30 20:27:24 -0600

    Set up class and constructor method

--- RestManGui/src/Check/Check.java
+++ RestManGui/src/Check/Check.java
@@ -3,12 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package Check;
+
+import Menu.*;
+import Table.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author garre
  */
 public class Check {
+    private int id;
+    private Table table;
+    private boolean open; //Open checks are true or closed are false
+    private double subtotal; //total price of all of the items
+    private double total; // final total
+    private ArrayList<MenuItem> items;
     
+    public Check(int id, Table table){
+        
+        this.id = id;
+        this.table = table;
+        this.open = true;
+        this.subtotal = 0.00;
+        this.total = subtotal;
+        
+        items = new ArrayList<MenuItem>();
+    }
 }
